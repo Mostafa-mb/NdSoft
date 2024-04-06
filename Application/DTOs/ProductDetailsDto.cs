4e8a175ac6b101dcfbe781dd5482a3a08363adc0
mostafa mobasher <mobashermostafa@gmail.com>
2024-04-06 16:01:00 +0330

    Add ProductDto

---
+++ Application/DTOs/ProductDetailsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DTOs
+{
+    public class ProductDetailsDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ProductDate { get; set; }
+        public string ManufacturePhone { get; set; }
+        public string ManufactureEmail { get; set; }
+    }
+}
