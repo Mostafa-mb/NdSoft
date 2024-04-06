c8e0d6a4023c46b22cb78a6e479c9c64913a3b6a
mostafa mobasher <mobashermostafa@gmail.com>
2024-04-06 14:46:54 +0330

    Add Models

---
+++ Domain/Entities/AppUserProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class AppUserProduct
+    {
+        public string AppUserId { get; set; }
+        public long ProductId { get; set; }
+        public AppUser AppUser { get; set; }
+        public Product product { get; set; }
+    }
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ProductDate { get; set; }
+        public string ManufacturePhone { get; set; }
+        public string ManufactureEmail { get; set; }
+        public bool IsAvailable { get; set; }
+
+
+        public List<AppUserProduct> AppUserProducts { get; set; } = new List<AppUserProduct>();
+    }
+}
