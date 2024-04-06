29e2e1cd9a596727eaec3c77baf7b5d6bea842cc
mostafa mobasher <mobashermostafa@gmail.com>
2024-04-06 14:58:10 +0330

    Add DbContext and configurate it

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext :IdentityDbContext<AppUser>
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<AppUserProduct> AppUserProduct { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<AppUserProduct>(p => p.HasKey(p => new { p.ProductId, p.AppUserId }));
+
+
+            builder.Entity<AppUserProduct>()
+                .HasOne(p => p.AppUser)
+                .WithMany(p => p.AppUserProducts)
+                .HasForeignKey(p => p.AppUserId);
+            
+            builder.Entity<AppUserProduct>()
+                .HasOne(p => p.product)
+                .WithMany(p => p.AppUserProducts)
+                .HasForeignKey(p => p.ProductId);
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,7 @@
 
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace WebApi
 {
     public class Program
@@ -13,6 +16,11 @@ namespace WebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnections"));
+            });
 
             var app = builder.Build();
 
