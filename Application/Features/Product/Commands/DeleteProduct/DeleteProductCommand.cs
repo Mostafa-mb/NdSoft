a7fe84ed64e94a43cc61fb7d7dffbc80925965c9
mostafa mobasher <mobashermostafa@gmail.com>
2024-04-06 21:09:25 +0330

    update command and command Handler

---
+++ Application/Features/Product/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,33 @@
+﻿using Application.DTOs;
+using Application.IRepositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Product.Commands.DeleteProduct
+{
+    public class DeleteProductCommand : IRequest
+    {
+        public ProductDetailsDto productDto { get; set; }
+    }
+
+
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteProductCommandHandler(IProductRepository productRepository , IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var selectedproduct = _mapper.Map<Product>(request.productDto);
+            await _productRepository.DeleteAsync(selectedproduct);
+            return selectedproduct;
+        }
+    }
+}
---
+++ Application/Features/Product/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,33 @@
+﻿using Application.DTOs;
+using Application.IRepositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Product.Commands.UpdateProduct
+{
+    public class UpdateProductCommand : IRequest
+    {
+        public ProductDetailsDto productDto { get; set; }
+    }
+
+
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateProductCommandHandler(IProductRepository productRepository , IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+
+        public Task Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var newProduct = _mapper.Map<Product>(request.productDto);
+            return newProduct;
+        }
+    }
+}
